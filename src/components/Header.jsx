88b6329a9a16558d77b20ae415e2fbd47fd34f81
alpetrushkin1984 <alpetrushkin1984@gmail.com>
2022-04-27 21:09:11 +0300

    unit14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://w7.pngwing.com/pngs/184/1023/png-transparent-logo-nelumbo-nucifera-lotus-miscellaneous-purple-text.png" alt=""/>
         </header>
     )
