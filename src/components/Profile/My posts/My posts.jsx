98bfe4e0c766795cf052e86c6d89c0f252e68b60
alpetrushkin1984 <alpetrushkin1984@gmail.com>
2022-04-28 21:01:28 +0300

    unit15

---
+++ src/components/Profile/My posts/My posts.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from './My posts.module.css';
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+                <button>Remove</button>
+            </div>
+            <div className={s.posts}>
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+            </div>
+        </div>
+    )
+}
+
+export default MyPosts;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import s from './Profile.module.css';
+import MyPosts from "./My posts/My posts";
+
 
 const Profile = () => {
     return (
@@ -10,20 +12,7 @@ const Profile = () => {
             <div>
                 ava + descr
             </div>
-            <div>
-                My posts
-                <div>
-                    New post
-                </div>
-                <div className={s.posts}>
-                    <div className={s.item}>
-                        post 1
-                    </div>
-                    <div className={s.item}>
-                        post 2
-                    </div>
-                </div>
-            </div>
+            <MyPosts />
         </div>
     )
 }
