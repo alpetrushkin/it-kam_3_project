c0595554316e721d613e0520b76f0f3cebc86cfa
alpetrushkin1984 <alpetrushkin1984@gmail.com>
2022-05-03 18:30:05 +0300

    unit19 Ok

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = (props) => {
   return (
@@ -13,8 +13,10 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-             <Route exact path={"/dialogs"} components={<Dialogs>}/>
-                 <Route path={"/profile"} components={<Profile>}/>
+             <Routes>
+                 <Route exact path={"/dialogs"} element={<Dialogs/>}/>
+                 <Route path={"/profile"} element={<Profile/>}/>
+             </Routes>
         </div>
     </div>
       </BrowserRouter>
